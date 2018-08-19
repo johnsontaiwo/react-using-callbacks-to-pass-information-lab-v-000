182886e491f6e9a9e66e1e3c4e04d35f85ad682f
Johnson Taiwo <josseytee@gmail.com>
2018-08-19 20:44:58 +0000

    automatically backed up by learn

--- src/Cell.js
+++ src/Cell.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 
 export default class Cell extends Component {
-  
+
   constructor(props) {
     super(props)
     this.state = {
@@ -20,5 +20,5 @@ export default class Cell extends Component {
       </div>
     )
   }
-  
+
 }
